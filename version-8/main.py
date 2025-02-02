ef59c1e7696541bd619f06928094ec51513fa9db
3akare <bakaredavid007@gmail.com>
2025-02-02 21:31:55 +0100

    feat: draw styled landmarks

--- version-8/main.py
+++ version-8/main.py
@@ -23,6 +23,37 @@ def draw_landmarks(image, results):
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw left hand landmarks
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw right hand landmarks
 
+def draw_styled_landmarks(image, results):
+    # Draw face landmarks
+     mp_drawing.draw_landmarks(
+             image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+             mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+             mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
+             )
+
+     # Draw pose landmarks
+     mp_drawing.draw_landmarks(
+             image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+             mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+             mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+             )
+
+     # Draw left hand landmarks
+     mp_drawing.draw_landmarks(
+             image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+             mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+             mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+)
+
+     # Draw right hand landmarks
+     mp_drawing.draw_landmarks(
+             image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+             mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+             mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+)
+
+#stopped at 40:42
+
 cap = cv2.VideoCapture(0) # open webcam
 
 with mp_holistic.Holistic( min_detection_confidence=0.5,  min_tracking_confidence=0.5 ) as holistic:
@@ -34,7 +65,7 @@ with mp_holistic.Holistic( min_detection_confidence=0.5,  min_tracking_confidenc
         image, results = mediapipe_detection(frame, holistic)
 
         # Draw landmarks
-        draw_landmarks(image, results)
+        draw_styled_landmarks(image, results)
 
         # Show to screen
         cv2.imshow("ASL Detection", image)
