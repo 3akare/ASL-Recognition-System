e7ece1ffde3d8b8bfea2d1692b1e3ecc2bd7aaed
3akare <bakaredavid007@gmail.com>
2025-02-02 21:13:37 +0100

    fix: changed FACE_CONNECTIONS to FACEMESH_TESSELATION

--- version-8/main.py
+++ version-8/main.py
@@ -18,7 +18,7 @@ def mediapipe_detection(image, model):
     return image, results
 
 def draw_landmarks(image, results):
-    # mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS) # Draw face landmarks
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION) # Draw face landmarks
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose landmarks
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw left hand landmarks
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw right hand landmarks
@@ -37,7 +37,7 @@ with mp_holistic.Holistic( min_detection_confidence=0.5,  min_tracking_confidenc
         draw_landmarks(image, results)
 
         # Show to screen
-        cv2.imshow("ASL-Detection", frame)
+        cv2.imshow("ASL Detection", image)
 
         # Exit gracefully
         if cv2.waitKey(1) & 0xFF == ord('q'):
